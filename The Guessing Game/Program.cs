8893ce061072a9985cbb867ea304e888da89b524
Nick Baesman <baesmanw@miamioh.edu>
2023-05-24 15:12:02 -0400

    loop finished

--- The Guessing Game/Program.cs
+++ The Guessing Game/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            
+
+
+
+            for (int i = 0; i < 3; i++) { 
+
+
             Console.WriteLine("Welcome to our guessing game \n it will be hard \n you may want to cry \n but don't worry you'll get it eventually");
 
             int theNumber = 7;
@@ -11,15 +18,28 @@
             Console.WriteLine("Please guess a number between 1 and 10");
             int userNumber = Convert.ToInt32(Console.ReadLine());
 
+
             if (userNumber == theNumber)
             {
                 Console.WriteLine("Congrats you are not an idiot that is correct!!!!!");
 
-            } else if (userNumber != theNumber) { Console.WriteLine($"You're an idiot of course it's not {userNumber} please try again"); }
+            }
+            else if (userNumber < 1 || userNumber > 10)
+            {
+                Console.WriteLine("the number you have chosen is outside the paramaters of the question \nPlease make sure it is a digit between 1 and 10");
+       
+            }
 
-            else if (userNumber < 1 || userNumber >10) { 
-                Console.WriteLine("the number you have chosen is outside the paramaters of the question \n Please make sure it is a number between 1 and 10"); 
-            } else { Console.WriteLine("Don't write the number out you idiot, use the digit"); }
+            else
+            {
+
+                Console.WriteLine($"You're an idiot of course it's not {userNumber} please try again");
+            }
+                Console.WriteLine("you're out of tries oops");
+            }
+
+
+
 
 
 
